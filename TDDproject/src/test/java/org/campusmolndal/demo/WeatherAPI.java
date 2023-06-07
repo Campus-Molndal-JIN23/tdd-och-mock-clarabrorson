d08bedf298dc714b1a66602a68b6dd173a81a2ad
clarabrorson <88clabro@gafe.molndal.se>
2023-06-07 10:47:07 +0200

    Update WeatherAPI.java
    lagt till if sats, throw och felmeddelande

--- TDDproject/src/test/java/org/campusmolndal/demo/WeatherAPI.java
+++ TDDproject/src/test/java/org/campusmolndal/demo/WeatherAPI.java
@@ -9,27 +9,29 @@ import java.io.BufferedReader;
 public class WeatherAPI {
 
     private final String apiKey = "f6afdf0f361d0dc9ff397c821a4ce2d3";
-    private final String requesturl = "http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid={f6afdf0f361d0dc9ff397c821a4ce2d3}";
+    private final String requestUrl = "http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid={f6afdf0f361d0dc9ff397c821a4ce2d3}";
 
     public WeatherAPI() {
     }
 
     public String getData() throws IOException {
-        URL url = new URL(requesturl);
+        URL url = new URL(requestUrl);
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
         connection.setRequestMethod("GET");
         connection.connect();
         int responseCode = connection.getResponseCode();
 
-        BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-        StringBuilder response = new StringBuilder();
-        String line;
-        while ((line = reader.readLine()) != null) {
-            response.append(line);
+        if (responseCode == HttpURLConnection.HTTP_OK) {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            StringBuilder response = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                response.append(line);
+            }
+            reader.close();
+            return response.toString();
+        } else {
+            throw new IOException("Failed to get data from API. HTTP response code: " + responseCode);
         }
-        reader.close();
-
-        return response.toString();
     }
 }
-
